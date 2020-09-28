4a22b045c698cd8da67ae2286812c8348c8672dc
Alex Markin <alexndr86@gmail.com>
2020-09-28 19:44:14 +0300

    Add banner description

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import Banner from './Banner';
 import requests from './requests';
 
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
---
+++ src/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <header> {/* << Background image */}
+      {/* title */}
+      {/* div > buttons */}
+      {/* description */}
+    </header>
+  )
+}
+
+export default Banner
