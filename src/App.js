9bdd26288f5df16c484dd459d141591b9c3c5f50
Alex Markin <alexndr86@gmail.com>
2020-09-29 13:40:47 +0300

    Add banner styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import Banner from './Banner';
-import requests from './requests';
-
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import Banner from "./Banner";
+import requests from "./requests";
 
 function App() {
   return (
@@ -14,21 +13,13 @@ function App() {
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now"
-        fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated"
-        fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies"
-        fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies"
-        fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies"
-        fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies"
-        fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries"
-        fetchUrl={requests.fetchDocumentaries} />
-
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
